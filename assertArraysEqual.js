89348a06af8089be721fc289de61968420f448b5
YuriMacBook <07yuri@gmail.com>
2022-04-11 08:35:55 -0400

    refactoring assertArraysEqual with modules

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,18 +1,11 @@
+const eqArrays = require('./eqArrays');
 
-const eqArrays = function (array1, array2) {
-  return (
-    array1.length === array2.length &&
-    array1.every((element, index) => {
-      return element === array2[index];
-    })
-  );
-};
-
-const assertArraysEqual = function (array1, array2) {
-  if (eqArrays(array1, array2)) {
+const assertArraysEqual = function(array1, array2) {
   const inspect = require("util").inspect;
+  if (eqArrays(array1, array2)) {
     return console.log(`🥒 Assertion Passed: ${inspect(array1)} === ${inspect(array2)}`);
   }
-  return console.log(`🌶 Assertion Failed: first array !== second array`);
+  return console.log(`🌶 Assertion Failed: ${inspect(array1)} !== ${inspect(array2)}`);
 };
-assertArraysEqual([1, 2, 3], [1, 2, "3"]); // => should FAIL
+
+module.exports = assertArraysEqual;
