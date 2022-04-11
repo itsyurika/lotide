abbdf211790e9f8bd9ccbf2f308406cb8fcd4699
YuriMacBook <07yuri@gmail.com>
2022-04-11 08:19:49 -0400

    adding module exports to assertEqual and head .js

--- assertEqual.js
+++ assertEqual.js
@@ -1,15 +1,10 @@
-// const pass = String.fromCodePoint(0x1f952);
-// const fail = String.fromCodePoint(0x1f336);
 
-const assertEqual = function (actual, expected) {
+const assertEqual = function(actual, expected) {
   if (actual === expected) {
     return console.log(`🥒 Assertion Passed: ${actual} === ${expected}`);
   }
   return console.log(`🌶 Assertion Failed: ${actual} !== ${expected}`);
 };
 
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-assertEqual(1, 4);
-assertEqual("hello", "hello");
-assertEqual("hello", "Hello");
+
+module.exports = assertEqual;
--- head.js
+++ head.js
@@ -1,17 +1,6 @@
-const pass = String.fromCodePoint(0x1f952);
-const fail = String.fromCodePoint(0x1f336);
-
-const assertEqual = function (actual, expected) {
-  if (actual === expected) {
-    console.log(`${pass} Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`${fail} Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
-const head = function (array) {
+const assertEqual = require('./assertEqual');
+const head = function(array) {
   return array[0];
 };
 
-assertEqual(head([]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+module.exports = head;
