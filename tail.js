039f4fa8804b70ade59d1a6174205036121a9bae
YuriMacBook <07yuri@gmail.com>
2022-04-11 08:24:02 -0400

    refactoring tail.js

--- tail.js
+++ tail.js
@@ -1,21 +1,6 @@
-const pass = String.fromCodePoint(0x1f952);
-const fail = String.fromCodePoint(0x1f336);
-
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`${pass} Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`${fail} Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
 
 const tail = function(array) {
   return array.slice(1);
 };
 
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2); // ensure we get back two elements
-assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(result[1], "Labs"); // ensure second element is "Labs"
-const words = [];
-console.log(tail(words));
+module.exports = tail;
